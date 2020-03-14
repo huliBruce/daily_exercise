c89accc4c4dc412492e6b6832e0d1f62a748f715
huli <huli@kuaishou.com>
2020-03-14 20:46:39 +0800

    samll changes

--- 20200314 close pairs.py
+++ 20200314 close pairs.py
@@ -1,47 +1,48 @@
 #https://www.youtube.com/watch?v=GBuHSRDGZBY
 #https://www.csdojo.io/problem
-#如何将最后获取的坐标和sort前的坐标对应起来
-#如何保留多个坐标
+#不需要获取坐标，只需要把两个数字找出来
+#起名字的时候尽量取的易懂一些
+#尽量定义函数
+#如何保留多个坐标？
+
 #    x x x x V
 #    x x x x x
 #    x x x x x
 #    x x x x x
 
-arrayA = [1,2,5]
-arrayB = [-2,0,1]
 
-keyNumber = 4
+def cloest_sum_pair(a1,a2,target):
+	a1_sorted = sorted(a1)
+	a2_sorted = sorted(a2)
+	keyY = 0
+	keyX = len(a2) - 1
+	smallest_diff = abs(a1_sorted[0] + a2_sorted[0] - target)
+	cloest_pair = (a1_sorted[0],a2_sorted[0])
 
-arrayA.sort()
-arrayB.sort()
+	while keyY < len(a1) and keyX >=0 :
+		v1 = a1_sorted[keyX]
+		v2 = a2_sorted[keyY]
+		current_diff = v1 + v2 - target
+		if abs(current_diff) < smallest_diff:
+			smallest_diff = abs(current_diff)
+			cloest_pair = (v1,v2)
 
-print(len(arrayA))
+		if current_diff == 0:
+			return cloest_pair
 
-
-lenArray = len(arrayA) 
-keyA = lenArray -1
-keyB = 0
-
-position = [-1,-1]
-minNumber = keyNumber
-
-
-for keyB in range(0,lenArray):
-	totalNumber = arrayA[keyA] + arrayB[keyB] 
-
-	if totalNumber == keyNumber:
-		position[0] = keyA
-		position[1] = keyB
-		break
-
-	if abs(totalNumber - keyNumber) <  minNumber:
-		position[0] = keyA
-		position[1] = keyB
-
-	if totalNumber > keyNumber:
-		if(keyA > 0):
-			keyA -= 1
+		if current_diff > 0:
+			keyX -= 1
 		else:
-			break
+			keyY += 1
+	return cloest_pair
 
-print(position)
+
+b1 = [7, 4, 1, 10]
+b2 = [4, 5, 8, 7]
+b_target = 13
+
+reuslt = cloest_sum_pair(b1,b2,b_target)
+
+print(reuslt)
+
+	
